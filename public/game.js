3f32423277ee474fdb94027cebe613936bc43e8b
Fiona Selanno <Fiona@MacBook-Pro-van-Fiona.local>
2016-07-13 20:16:30 +0200

    created function ball

--- public/game.js
+++ public/game.js
@@ -58,3 +58,19 @@ Player.prototype.render = function() {
 Computer.prototype.render = function() {
   this.bat.render();
 };
+
+
+function Ball(x, y) {
+  this.x = x;
+  this.y = y;
+  this.x_speed = 0;
+  this.y_speed = 3;
+  this.radius = 5;
+}
+
+Ball.prototype.render = function() {
+  context.beginPath();
+  context.arc(this.x, this.y, this.radius, 2 * Math.PI, false);
+  context.fillStyle = "#fff";
+  context.fill();
+};
