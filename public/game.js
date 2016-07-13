0fa544a859c2b87d056bea746575f340e543d3de
Fiona Selanno <Fiona@MacBook-Pro-van-Fiona.local>
2016-07-13 17:16:14 +0200

    set canvas and grid

--- public/game.js
+++ public/game.js
@@ -2,3 +2,10 @@ var animate = window.requestAnimationFrame ||
   window.webkitRequestAnimationFrame ||
   window.mozRequestAnimationFrame ||
   function(callback) { window.setTimeout(callback, 1000/60) };
+
+var canvas = document.createElement('canvas');
+var width = 600;
+var height = 400;
+canvas.width = width;
+canvas.height = height;
+var context = canvas.getContext('2d');
