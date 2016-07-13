7e842fc919b6180334d8d75e24946bea7e07802e
Fiona Selanno <Fiona@MacBook-Pro-van-Fiona.local>
2016-07-13 21:28:03 +0200

    position bats left and right

--- public/game.js
+++ public/game.js
@@ -51,10 +51,10 @@ Bat.prototype.render = function(){
 
 
 function Player(){
-  this.bat = new Bat(275, 380, 10, 50);
+  this.bat = new Bat(580, 175, 10, 50);
 };
 function Computer(){
-  this.bat = new Bat(275, 10, 10, 50);
+  this.bat = new Bat(10, 175, 10, 50);
 };
 
 Player.prototype.render = function() {
