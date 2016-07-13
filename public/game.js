ed9ac94058070e9fc56b66a77521c38f11212c64
Fiona Selanno <Fiona@MacBook-Pro-van-Fiona.local>
2016-07-13 22:24:10 +0200

    right bat bouncing back

--- public/game.js
+++ public/game.js
@@ -81,9 +81,44 @@ Ball.prototype.render = function() {
   context.fill();
 };
 
-Ball.prototype.update = function(){
+Ball.prototype.update = function(bat1, bat2){
   this.x += this.x_speed;
   this.y += this.y_speed;
+  var top_x = this.x - 5;
+  var top_y = this.y - 5;
+  var bottom_x = this.x + 5;
+  var bottom_y = this.y + 5;
+
+  if(this.x - 5 < 0) { // hitting the left wall
+    this.x = 5;
+    this.x_speed = -this.x_speed;
+  } else if(this.x + 5 > 582) { // hitting the right wall
+    this.x = 577;
+    this.x_speed = -this.x_speed;
+  }
+
+  // if(this.y < 0 || this.y > 600) { // a point was scored
+  //   this.x_speed = 0;
+  //   this.y_speed = 3;
+  //   this.x = 200;
+  //   this.y = 300;
+  // }
+  //
+  // if(top_y > 300) {
+  //   if(top_y < (bat1.y + bat1.height) && bottom_y > bat1.y && top_x < (bat1.x + bat1.width) && bottom_x > bat1.x) {
+  //     // hit the player's paddle
+  //     this.y_speed = -3;
+  //     this.x_speed += (bat1.x_speed / 2);
+  //     this.y += this.y_speed;
+  //   }
+  // } else {
+  //   if(top_y < (bat2.y + bat2.height) && bottom_y > bat2.y && top_x < (bat2.x + bat2.width) && bottom_x > bat2.x) {
+  //     // hit the computer's paddle
+  //     this.y_speed = 3;
+  //     this.x_speed += (bat2.x_speed / 2);
+  //     this.y += this.y_speed;
+  //   }
+  // }
 };
 
 var player = new Player();
